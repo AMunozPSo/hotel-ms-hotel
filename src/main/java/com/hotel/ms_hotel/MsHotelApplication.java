beb51d9568772a15f11664a61584f2c9b111db5c
Antonia Muñoz <antonia.sof.mp@gmail.com>
2026-04-28 18:32:15 -0400

    microservicio finalizado

--- src/main/java/com/hotel/ms_hotel/MsHotelApplication.java
+++ src/main/java/com/hotel/ms_hotel/MsHotelApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsHotelApplication {
+public class 	MsHotelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsHotelApplication.class, args);
