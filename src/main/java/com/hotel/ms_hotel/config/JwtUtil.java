ec42a8fa8f18e69f9bd450ae7918ea015bb27edd
Antonia Muñoz <antonia.sof.mp@gmail.com>
2026-05-14 13:12:07 -0400

    Implementacion de jwt en config, añadi dependencias en el pom y security en config

---
+++ src/main/java/com/hotel/ms_hotel/config/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.hotel.ms_hotel.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    // Misma contraseña del otro microservicio
+    private static final String SECRET ="hotel-microservicios-secret-key-2024-segura";
+
+    private SecretKey getKey(){
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public boolean isValid(String token) {
+        try {
+            Jwts.parser().verifyWith(getKey()).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey()).build()
+                .parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+
+    public String getRol(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey()).build()
+                .parseSignedClaims(token)
+                .getPayload().get("rol", String.class);
+    }
+}
---
+++ src/main/java/com/hotel/ms_hotel/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.hotel.ms_hotel.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // Ruta pública para ver hoteles (GET)
+                        .requestMatchers(HttpMethod.GET, "/api/v1/hoteles/**").permitAll()
+
+                        // Todo lo demás requiere Token (POST, DELETE, PUT)
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
